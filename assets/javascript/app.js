e7dcacf7e5d975e9880e4edd4781ab035ce64a5e
David Cardenas <davidcardenas1993@yahoo.com>
2019-09-21 21:14:36 -0600

    completed and tested game logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,6 +13,26 @@ var trivQuestions = [{
     question: "In automotive terms, what does RPM stand for?",
     choices: ["Road Power Modulator", "Resale Price Maintenance", "Revolutions Per Minute", "None of the Above"],
     answer: "Revolutions Per Minute"
+},
+{
+    question: "What does antifreeze do?",
+    choices: ["Make the car go vroom vroom", "Keep the engine cool", "Make a tasty drink", "None of the Above"],
+    answer: "Keep the engine cool"
+},
+{
+    question: "Where does engine oil go?",
+    choices: ["In the trunk", "In the headlight", "Inside the engine", "None of the Above"],
+    answer: "Inside the engine"
+},
+{
+    question: "What should you do when your check engine light comes on?",
+    choices: ["Put tape over the light", "Take it to a trusted mechanic", "Open the hood and see if the engine is there", "None of the Above"],
+    answer: "Take it to a trusted mechanic"
+},
+{
+    question: "How often should you change your blinker fluid?",
+    choices: ["It's a lifetime fluid", "Once a month", "Once a year", "There is no such thing as blinker fluid"],
+    answer: "There is no such thing as blinker fluid"
 }
 ];
 
@@ -28,7 +48,8 @@ var newQuestion = function() {
 
     var questionEnd = (trivQuestions.length - 1) === presQuestion; 
     if (questionEnd) {
-        console.log("Game Over!")
+        console.log("Game Over!");
+        showOutCome();
     } else {
         presQuestion++;
         showQuestion();
@@ -64,9 +85,11 @@ var showQuestion = function () {
     var choices = trivQuestions[presQuestion].choices;
 
     $('#Timer').html('Timer: ' + counter);
-    $('#Gameplay').html(`<h4>${question}</h4>
-    ${showChoices(choices)}`
-    );
+    $('#Gameplay').html(`
+    <h4>${question}</h4>
+    ${showChoices(choices)}
+    ${questRem()}
+    `);
     
 }
 
@@ -88,16 +111,50 @@ $(document).on('click', '.choice', function() {
     var answer = trivQuestions[presQuestion].answer;
     
     if (answer === selection) {
-        //TODO
-        //USER WINS
         score++;
         alert("Correct!!");
         newQuestion();
     } else {
         loss++;
-        alert("Incorrect");
+        alert(`Incorrect. The correct answer is ${answer}`);
         newQuestion();
     }
 });
 
-showQuestion();
+//I want to show the user's stats at the end of the game
+var showOutCome = function() {
+    var outCome = `
+        <p>You answered ${score} questions(s) correctly and</p>
+        <p>you answered ${loss} questions(s) incorrectly</p>
+        <p>out of a total amount of ${trivQuestions.length} questions(s)</p>
+        <button class="btn btn-primary" id="restart">Restart Game</button>
+    `; 
+
+    $('#Gameplay').html(outCome);
+};
+
+//add button to restart the game
+$(document).on('click', '#restart', function() {
+    counter = 30;
+    presQuestion = 0;
+    score = 0; 
+    loss = 0;
+    timer = null;
+
+    showQuestion();
+});
+
+//Below is the question counter for user visual
+var questRem = function() {
+    var remQuest = trivQuestions.length - (presQuestion + 1);
+    var totQuest = trivQuestions.length;
+
+    return `Rem Quest: ${remQuest}/${totQuest}`;
+}
+
+//This begin function allows the user to begin the game by clicking the button at the top of the page.
+$('#Begin').click(function() {
+    $('#Begin').remove();
+    $('#Timer').html(counter);
+    showQuestion();
+})
